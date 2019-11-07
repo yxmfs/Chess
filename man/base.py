1c1aabd5ca50df835b3605f41ed19597ef37e13b
ygm_tencent <ygm@tencentcloud.com>
2019-11-07 18:06:51 +0800

    add a function to check if the man is out of its board and add other man

--- man/base.py
+++ man/base.py
@@ -25,6 +25,24 @@ class BaseMan():
             return False
         else:
             return True
+    def isOut(self,pos):
+        min_x = conf_dict['min_x']
+        max_x = conf_dict['max_x']
+        tmp = int(conf_dict['max_y']/2)
+        if self.get_color() == 'r':
+            min_y = conf_dict['min_y']
+            max_y = tmp
+        if self.get_color() == 'b':
+            min_y = tmp+1
+            max_y = conf_dict['max_y']
+
+        if (pos[0]>max_x) or (pos[0]<min_x):
+            return True
+        elif (pos[1]>max_y) or (pos[1]<min_y):
+            return True
+        else:
+            return False
+
     def nextsteps(self):
         nextList = []
         x = self.pos[0] + 1
@@ -68,7 +86,81 @@ class BossMan(BaseMan):
 class CarMan(BaseMan):
     def __init__(self,pos,color):
         BaseMan.__init__(self,pos,color,'car')
- 
+    def nextsteps(self):
+        nextList = []
+        x = self.pos[0] + 1
+        y = self.pos[1]
+        if self.isLegal([x,y]):
+            nextList.append([x,y])
+        x = self.pos[0]
+        y = self.pos[1] + 1
+        if self.isLegal([x,y]):
+            nextList.append([x,y])
+        x = self.pos[0] - 1
+        y = self.pos[1]
+        if self.isLegal([x,y]):
+            nextList.append([x,y])
+        x = self.pos[0]
+        y = self.pos[1] - 1
+        if self.isLegal([x,y]):
+            nextList.append([x,y])
+        return nextList
+class HorseMan(BaseMan):
+    def __init__(self,pos,color):
+        BaseMan.__init__(self,pos,color,'horse')
+    def nextsteps(self):
+        nextList = []
+        tmp_list = [(1,2),(2,1),(1,-2),(-2,1),
+                    (-1,2),(2,-1),(-1,-2),(-2,-1)]
+        for key in tmp_list:
+            x = self.pos[0] + key[0]
+            y = self.pos[1] + key[1]
+            if self.isLegal([x,y]):
+                nextList.append([x,y])
+        return nextList
+class MinisMan(BaseMan):
+    def __init__(self,pos,color):
+        BaseMan.__init__(self,pos,color,'minister')
+    def nextsteps(self):
+        nextList = []
+        tmp_list = [(2,2),(2,-2),(-2,2),(-2,-2)]
+        for key in tmp_list:
+            x = self.pos[0] + key[0]
+            y = self.pos[1] + key[1]
+            if self.isLegal([x,y]):
+                if not self.isOut([x,y]):
+                    nextList.append([x,y])
+        return nextList
+class GuardMan(BaseMan):
+    def __init__(self,pos,color):
+        BaseMan.__init__(self,pos,color,'guard')
+    def nextsteps(self):
+        nextList = []
+        tmp_list = [(1,1),(1,-1),(-1,1),(-1,-1)]
+        for key in tmp_list:
+            x = self.pos[0] + key[0]
+            y = self.pos[1] + key[1]
+            if self.isLegal([x,y]):
+                nextList.append([x,y])
+        return nextList
+class SoldierMan(BaseMan):
+    def __init__(self,pos,color):
+        BaseMan.__init__(self,pos,color,'soldier')
+    def nextsteps(self):
+        nextList = []
+        x = self.pos[0]
+        tmp_dict = {'r':1,'b':-1}
+        y = self.pos[1] + tmp_dict[self.get_color()]
+        if self.isLegal([x,y]):
+            nextList.append([x,y])
+        if self.isOut(self.pos):
+            tmp_list = [(1,0),(-1,0)]
+            for key in tmp_list:
+                x = self.pos[0] + key[0]
+                y = self.pos[1] + key[1]
+                if self.isLegal([x,y]):
+                    nextList.append([x,y])
+        return nextList
 if __name__ == '__main__':
     import sys
     sys.path.append('../')
